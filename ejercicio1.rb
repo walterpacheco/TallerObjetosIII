335e978304f00b48624dc9705c0d1f40aaaf2947
Walter Pacheco <walter.e.pacheco.chandia@gmail.com>
2019-05-16 14:31:10 -0400

    Se corrigen nombre de clase y llamado de metos

---
+++ ejercicio1.rb
@@ -0,0 +1,11 @@
+class MiClase
+    def self.de_instancia
+        puts 'Método de instancia!'
+    end
+    def self.de_clase
+        puts 'Método de clase!'
+    end
+end
+
+MiClase.de_instancia
+MiClase.de_clase
